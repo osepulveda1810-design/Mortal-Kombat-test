28ac4d643d896d68aadee96461c2f2d3b40bb5f2
osepulveda1810-design <o.sepulveda1810@gmail.com>
2026-07-23 21:04:33 -0400

    Actualizar service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'subzero-v1-51-fix-comenzar';
+const CACHE_NAME = 'subzero-v1-57-celeste-sprites';
 self.addEventListener('install', e=>{ self.skipWaiting(); });
 self.addEventListener('activate', e=>{
   e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
